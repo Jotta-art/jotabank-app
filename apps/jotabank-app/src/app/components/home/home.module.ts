0ae5fb026e58326b3eb943b9eaff7e736cc50935
Jotta-Art <joao.pedro42@outlook.com.br>
2023-03-01 07:00:52 -0300

    Adicionando primeng, primeflex e NX ao angular

---
+++ apps/jotabank-app/src/app/components/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './component/home.component';
+import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule],
+  exports: [HomeComponent],
+})
+export class HomeModule {}
