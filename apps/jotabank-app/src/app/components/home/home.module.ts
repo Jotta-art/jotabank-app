f1ee24f7772f46a4ce47075dd1c578abe9602c95
Jotta-Art <joao.pedro42@outlook.com.br>
2023-03-11 20:27:14 -0300

    Ajustando card inicial para usuário logar e seu estilo

--- apps/jotabank-app/src/app/components/home/home.module.ts
+++ apps/jotabank-app/src/app/components/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './component/home.component';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+    imports: [CommonModule, HomeRoutingModule, CardModule, ButtonModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
