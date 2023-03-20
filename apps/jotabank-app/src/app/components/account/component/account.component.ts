56d873fc4e5c3011e4a10af5bdb31f4396fce8c7
Jotta-Art <joao.pedro42@outlook.com.br>
2023-03-20 11:37:14 -0300

    Adicionando transição entre abas

--- apps/jotabank-app/src/app/components/account/component/account.component.ts
+++ apps/jotabank-app/src/app/components/account/component/account.component.ts
@@ -4,16 +4,26 @@ import {MenuItem} from "primeng/api";
 @Component({selector: 'app-account', templateUrl: 'account.component.html', styleUrls: ['account.component.scss']})
 export class AccountComponent implements OnInit {
   items: MenuItem[] = [];
+  showAbaConta = true;
+  showAbaCartoes = false;
 
   ngOnInit(): void {
     this.items = [
       {
         label: 'Sua Conta',
         icon: 'pi pi-fw pi-file',
+        command: () => {
+          this.showAbaConta = true;
+          this.showAbaCartoes = false;
+        }
       },
       {
         label: 'Seus Cartões',
         icon: 'pi pi-fw pi-pencil',
+        command: () => {
+          this.showAbaCartoes = true;
+          this.showAbaConta = false;
+        }
       },
       {
         label: 'Investimentos',
